b6165507ba3b555478a8924dcabc20fc8175adc0
alexrey1998 <alexis.reyboubet@ik.me>
2025-02-22 00:10:36 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,8 +16,23 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  // Pour les requêtes de navigation et pour le fichier script.js, utiliser une stratégie réseau en priorité.
+  if (event.request.mode === 'navigate' || event.request.url.endsWith('script.js')) {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          // Mettez à jour le cache avec la réponse fraîche
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseClone));
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+  } else {
+    // Pour les autres ressources, utiliser le cache si disponible
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => response || fetch(event.request))
+    );
+  }
 });
