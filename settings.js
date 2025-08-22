6609bd6e11bf783d551425b419ad966e6f157e0c
alexrey1998 <alexis.reyboubet@ik.me>
2025-08-22 12:31:32 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,13 +4,19 @@ export const settings = {
   stationboardLimit: 50,
 
   // Période max d’affichage des départs (hh:mm:ss)
-  // ex. 01:30:00 = 1h30
   maxDisplayPeriod: "01:30:00",
 
   // Limite courte pour l’ajustement de destination
   adjustLookupLimit: 5,
 
   // Intervalle de rafraîchissement (hh:mm:ss)
-  // ex. 00:01:00 = 1 minute
-  refreshInterval: "00:01:00"
+  refreshInterval: "00:01:00",
+
+  // Apparence de la partie après la virgule (suffixe) des noms d’arrêts
+  stopSuffix: {
+    // Taille du suffixe en % par rapport au texte normal (ex: 75 => 75%)
+    sizeRatioPercent: 75,
+    // Couleur du suffixe : "default" (hérite/noir actuel), ou "blue", "green", "#000000", etc.
+    color: "default"
+  }
 };
