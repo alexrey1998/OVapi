f7f24c69d38cbbbeb60618244cf89eb4d8aa2cdf
alexrey1998 <alexis.reyboubet@ik.me>
2025-08-22 12:11:02 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,9 +3,14 @@ export const settings = {
   // Nombre de départs à récupérer auprès de l’API (utilisé partout)
   stationboardLimit: 50,
 
-  // Fenêtre max d’affichage en minutes
-  maxDisplayMinutes: 90,
+  // Période max d’affichage des départs (hh:mm:ss)
+  // ex. 01:30:00 = 1h30
+  maxDisplayPeriod: "01:30:00",
 
   // Limite courte pour l’ajustement de destination
-  adjustLookupLimit: 5
+  adjustLookupLimit: 5,
+
+  // Intervalle de rafraîchissement (hh:mm:ss)
+  // ex. 00:01:00 = 1 minute
+  refreshInterval: "00:01:00"
 };
