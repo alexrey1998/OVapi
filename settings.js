ded9962a4143bd24082f6bde21a6455a6d06d4fe
alexrey1998 <alexis.reyboubet@ik.me>
2025-08-22 12:03:51 +0200

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,11 @@
+// settings.js
+export const settings = {
+  // Nombre de départs à récupérer auprès de l’API (utilisé partout)
+  stationboardLimit: 50,
+
+  // Fenêtre max d’affichage en minutes
+  maxDisplayMinutes: 90,
+
+  // Limite courte pour l’ajustement de destination
+  adjustLookupLimit: 5
+};
