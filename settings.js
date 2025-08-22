d97bf366c2763bf0cb379796d41c457b8e72bf52
alexrey1998 <alexis.reyboubet@ik.me>
2025-08-22 14:59:09 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 export const settings = {
   // Nombre de départs à récupérer auprès de l’API stationboard
-  stationboardLimit: 50,
+  stationboardLimit: 2,
 
   // Période d’affichage max des départs (hh:mm:ss)
   maxDisplayPeriod: "01:30:00",
@@ -15,6 +15,6 @@ export const settings = {
     prefixScalePct: 80,
     // Couleur du SUFFIXE (et de tout le nom s’il n’y a pas de virgule).
     // "default" = couleur par défaut du texte. Sinon, toute valeur CSS valide "blue", "#008000", etc.
-    suffixColor: "#2d327d"
+    suffixColor: "green"
   }
 };
